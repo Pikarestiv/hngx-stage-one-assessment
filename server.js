4d3d2c7cb1ca8d1acba657f84dd062e07980e79b
pikarestiv <okwyodumodu@gmail.com>
2023-09-10 11:07:29 +0100

    Add server files

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const moment = require("moment");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/api", (req, res) => {
+  const { slack_name, track } = req.query;
+
+  // Get current day of the week
+  const current_day = moment().format("dddd");
+
+  // Get current UTC time
+  const utc_time = moment().utc().format();
+
+  // GitHub URLs
+  const github_repo_url = "https://github.com/pikarestiv/hngx-stage-one-assessment";
+  const github_file_url = `${github_repo_url}/`;
+
+  const response = {
+    slack_name,
+    current_day,
+    utc_time,
+    track,
+    github_file_url,
+    github_repo_url,
+    status_code: 200,
+  };
+
+  res.json(response);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
